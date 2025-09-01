ad8b14f749e5bd59d609ad1b8a88b2da3f272455
batraronit07-ai <batra.ronit07@gmail.com>
2025-09-01 16:43:14 +0530

    class experiments

---
+++ class/17.c
@@ -0,0 +1,18 @@
+// WAP a program to count down from 3 to 
+
+#include<stdio.h>
+
+int main () {
+
+    int i;
+
+    for ( i = 3 ; i >= 0 ; i = i - 1)
+    {
+        printf("%d\n", i);
+
+    }
+    
+    printf("Happy New Year");
+    return 0;
+    
+}
